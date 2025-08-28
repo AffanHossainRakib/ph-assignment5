aa23766c2829c486ffb56988039e02875455f99a
Affan Hossain Rakib <affanhossainrakib@gmail.com>
2025-08-29 00:28:40 +0600

    feat: copy button functionalities

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// Common functions start
 function getElementValueByIdInt(elementId) {
   const elementInt = parseInt(document.getElementById(elementId).innerText);
   return elementInt;
@@ -5,13 +6,31 @@ function getElementValueByIdInt(elementId) {
 
 function setElementValueById(elementId, value) {
   const element = document.getElementById(elementId);
-  if (element) {
-    element.innerText = value;
-  }
+  element.innerText = value;
 }
 
+// Common functions end
+
 // Handling heart icon interaction
-function incrementHeartCount() {
-  const currentHeartCount = getElementValueByIdInt("heartCount");
-  setElementValueById("heartCount", currentHeartCount + 1);
-}
+const heartButtons = document.querySelectorAll(".cardHeart");
+heartButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const currentHeartCount = getElementValueByIdInt("globalHeartCount");
+    setElementValueById("globalHeartCount", currentHeartCount + 1);
+  });
+});
+
+// Handling copy button interaction
+const copyButtons = document.querySelectorAll(".copyButton");
+copyButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const currentGlobalCopyCount = getElementValueByIdInt("globalCopyCount");
+    setElementValueById("globalCopyCount", currentGlobalCopyCount + 1);
+
+    const parent2 = button.parentElement.parentElement; // Going two steps back
+    const phoneNumber = parent2.querySelector(".cardPhoneNumber").innerText;
+    navigator.clipboard.writeText(phoneNumber).then(() => {
+      alert("Phone number copied to clipboard: " + phoneNumber);
+    });
+  });
+});
